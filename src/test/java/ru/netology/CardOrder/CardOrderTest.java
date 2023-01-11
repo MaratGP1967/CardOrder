36791df7256e4a14e90477b030a514bf1c6de7cc
Marat Field <mrpolyansky@yandex.ru>
2023-01-11 23:13:55 +0300

    Changed code and build_gradle

--- src/test/java/ru/netology/CardOrder/CardOrderTest.java
+++ src/test/java/ru/netology/CardOrder/CardOrderTest.java
@@ -1,10 +1,8 @@
 package ru.netology.CardOrder;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -18,8 +16,8 @@ public class CardOrderTest {
 
     @BeforeAll
     static void setupAll() {
-        System.setProperty("webdriver.chrome.driver", ".driver/win/chromedriver.exe");
         WebDriverManager.chromedriver().setup();
+        System.setProperty("webdriver.chrome.driver", "./driver/win/chromedriver.exe");
     }
 
     @BeforeEach
@@ -46,6 +44,46 @@ public class CardOrderTest {
         driver.findElement(By.className("button")).click();
         String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
         String actual = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
+
         assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldNotTestNameCardOrder() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Ivan Petrov");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79625013494");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.className("button")).click();
+        Boolean expected = true;
+        Boolean actual = driver.findElement(By.className("input_invalid")).isDisplayed();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldNotTestPhoneCardOrder() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Иван Петров");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+796250134j4");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.className("button")).click();
+        Boolean expected = true;
+        Boolean actual = driver.findElement(By.className("input_invalid")).isDisplayed();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldNotTestCheckBoxCardOrder() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Иван Петров");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79625013494");
+        //driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.className("button")).click();
+
+        Assertions.assertThrows(NoSuchElementException.class, () -> {
+            driver.findElement(By.className("checkbox_checked")).isDisplayed();
+        });
     }
 }
